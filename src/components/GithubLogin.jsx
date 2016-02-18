7cf1b415d992dcaa6da67b61936dcbc4b5b8414e
David Cox <losttime.shuffle@gmail.com>
2016-02-17 21:25:55 -0700

    Exchange code for token

--- src/components/GithubLogin.jsx
+++ src/components/GithubLogin.jsx
@@ -13,7 +13,16 @@ module.exports = React.createClass({
 			var code = url.search.split('=')[1];
 			console.log('code', code);
 			// send `code` to our app backend
-			// expose token to parent somehow
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', 'http://localhost:3080/exchange/github');
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.onload = function(event) {
+				if (this.status == 200) {
+					var data = JSON.parse(this.response);
+					console.log('token', data.token);
+				}
+			}
+			xhr.send('{"code":"' + code + '"}');
 		}
 	},
 	render: function() {

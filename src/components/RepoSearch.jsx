8f94890bb5f607242c47641efe43d103e5e64cba
David Cox <losttime.shuffle@gmail.com>
2016-02-27 10:46:34 -0700

    Make repos searchable

--- src/components/RepoSearch.jsx
+++ src/components/RepoSearch.jsx
@@ -3,30 +3,48 @@ import SearchInput from 'react-search-input'
 
 import Repo from './Repo';
 
-let dummyRepo = {
+let dummyRepos = [{
 	name: 'octocat/HelloWorld',
 	owner: 'octocat',
 	issues: 50,
 	stars: 10,
 	watchers: 20
-}
+}, {
+	name: 'dconstructing/contribute',
+	owner: 'dconstructing',
+	issues: 0,
+	stars: 1,
+	watchers: 2
+}];
 
 let fakeItem = [];
 
 export default class RepoSearch extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			foundRepos: dummyRepos
+		};
 	}
 
 	search(query) {
 		console.log('search', query);
+		this.setState({
+			foundRepos: dummyRepos.filter((repo) => {
+				return repo.name.includes(query);
+			})
+		});
 	}
 
 	render() {
 		return (
 			<div>
 				<SearchInput ref='search' onChange={this.search.bind(this)} />
-				<Repo repo={dummyRepo} />
+				<div>
+					{ this.state.foundRepos.map(function(repo) {
+						return <Repo key={repo.name} repo={repo} />
+					})}
+				</div>
 			</div>
 		);
 	}
